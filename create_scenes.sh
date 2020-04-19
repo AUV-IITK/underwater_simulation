80280f27502c683f17199c52e66d8983d21b58bb
Rishabh Dugaye <58358984+rishabhd786@users.noreply.github.com>
2020-04-19 17:11:46 +0530

    Update create_scenes.sh

--- create_scenes.sh
+++ create_scenes.sh
@@ -4,7 +4,7 @@ echo $UWSIM_DATA_DIR
 DATA_DIR=./data/
 
 mkdir -p $UWSIM_DATA_DIR/data/terrain/pool
-cp $DATA_DIR/* $UWSIM_DATA_DIR/data/terrain/pool/
-
+cp $DATA_DIR/pool.osgt $UWSIM_DATA_DIR/data/terrain/pool/
+cp $DATA_DIR/*.osg $UWSIM_DATA_DIR/data/objects/
 echo "Copying done"
 
